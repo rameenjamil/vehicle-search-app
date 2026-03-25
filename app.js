d8a42afb9fcd021fb0952149024f764bc619c28e
Rameen Jamil <jamilrameen@gmail.com>
2026-03-25 17:47:04 +0000

    Add form submission handling and filtering by make, fuel type, and price

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', start);
+var searchForm = document.forms.searchForm
 
 var data = getData()
 
@@ -6,10 +7,10 @@ function start() {
     console.log('Page loaded');
 
     data.then(function (cars) {
-        console.log(cars);
+        // console.log(cars);
         populateMakeDropdown(cars)
     })
-
+    searchForm.addEventListener('submit', handleFormSubmission);
 }
 
 function getData() {
@@ -28,8 +29,44 @@ function populateMakeDropdown(cars) {
     makeList = new Set(makeList) //removing duplicates from the list of makes
     makeList.forEach(function (make) {  //populating the dropdown with the makes
         var option = document.createElement('option');
-        option.value = make;
+        option.value = make.toLowerCase();
         option.innerText = make;
         makeSelect.appendChild(option);
     })
+}
+
+function handleFormSubmission(e) {
+    console.log('Form submitted');
+    e.preventDefault(); //preventing the form from submitting and refreshing the page
+
+    var selectedMake = document.getElementById('make').value;
+    var selectedFuel = document.querySelector('input[name="fuel"]:checked').value;
+    var minPrice = Number(document.getElementById('minPrice').value) || 0; //if the user doesn't enter a value, it will default to 0
+    var maxPrice = Number(document.getElementById('maxPrice').value) || Infinity
+
+    data.then(function (cars) {
+        var results = filterCars(cars, selectedMake, selectedFuel, minPrice, maxPrice);
+        console.log(results);
+    })
+}
+
+function filterCars(cars, make, fuel, minPrice, maxPrice) {
+
+    make = make.toLowerCase();
+    fuel = fuel.toLowerCase();
+
+    return cars.filter(function (car) {
+        // console.log("selected make: ", make)  commented out the console logs for debugging purposes, but they can be uncommented if needed to check the values of the variables
+        // console.log("car make: ", car.make)
+        // console.log("selected fuel: ", fuel)
+        // console.log("car fuel: ", car.fuelType)
+
+
+        var matchesMake = make === '' || car.make.toLowerCase() === make;
+        var matchesFuel = fuel === '' || car.fuelType.toLowerCase() === fuel;
+        var matchesPrice = car.price >= minPrice && car.price <= maxPrice
+
+        return matchesMake && matchesFuel && matchesPrice;
+    })
+
 }
